50197a332773c0bc9fe12f4d81119682bcf744a9
weikun <wk2003119@163.com>
2015-12-15 09:31:45 +0800

    7.1

--- D2/src/com/weikun/h/C.java
+++ D2/src/com/weikun/h/C.java
@@ -4,7 +4,6 @@ import java.lang.reflect.Array;
 
 public class C {
 	
-	
 	public static void main(String[] args) {
 		//String [] ss=new String[]{"ABC","C","DEF"};
 		Object o=Array.newInstance(String.class, 3);
